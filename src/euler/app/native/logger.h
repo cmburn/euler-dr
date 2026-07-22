99517f12e276dd1398d7aace94ee4ab999b554c4
cmburn <33696630+cmburn@users.noreply.github.com>
2026-07-22 15:16:38 -0500

    Native backend work

--- src/euler/app/native/logger.h
+++ src/euler/app/native/logger.h
@@ -3,10 +3,24 @@
 #ifndef EULER_APP_NATIVE_LOGGER_H
 #define EULER_APP_NATIVE_LOGGER_H
 
-#include "euler/util/object.h"
+#include "euler/util/logger.h"
 
 namespace euler::app::native {
-class Logger final : public util::Object { };
+class Logger final : public util::Logger {
+public:
+	[[nodiscard]] const std::string &subsystem() const override;
+	void set_subsystem(std::string_view name) override;
+	[[nodiscard]] Severity severity() const override;
+	void set_severity(Severity level) override;
+	[[nodiscard]] util::Reference<util::Logger> copy(
+	    std::optional<std::string_view> subsystem) const override;
+
+protected:
+	void write_log(Severity level,
+	    const std::string &message) const override;
+
+private:
+};
 } /* namespace euler::app::native */
 
 
